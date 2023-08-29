ad3fab2b80118f5733665c2d379e55e01818ca75
emdinga <emdinga@gmail.com>
2023-08-29 06:53:31 -0700

    Binary_trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,39 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Size of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t right_size, left_size;
+
+	if (tree == NULL)
+		return (0);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+	return (1 + left_size + right_size);
+}
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
+	return (1 + (left_h > right_h ? left_h : right_h));
+}
+
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,16 +8,14 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (node == NULL || node->parent == NULL)
-                return (NULL);
-        if (node->parent->left == node)
-                return (node->parent->right);
-        if (node->parent->right == node)
-                return (node->parent->left);
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	if (node->parent->right == node)
+		return (node->parent->left);
+	return (NULL);
 }
-
-
 /**
  * binary_tree_uncle - Finds the uncle of a node
  * @node: Pointer to the node to find the uncle
