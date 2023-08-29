5e6e86c6b405899a11bd648ab4f980579c963f46
emdinga <emdinga@gmail.com>
2023-08-29 06:29:46 -0700

    Binary Tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,14 +8,11 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t left_size;
-	size_t right_size;
+	size_t right_size, left_size;
 
 	if (tree == NULL)
 		return (0);
-	
 	left_size = binary_tree_size(tree->left);
 	right_size = binary_tree_size(tree->right);
-
 	return (1 + left_size + right_size);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -10,6 +10,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NUll || func == NULL)
 		return;
 	binary_tree_postorder(tree->left, func);
-	binary_tree-postorder(tree->right, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
